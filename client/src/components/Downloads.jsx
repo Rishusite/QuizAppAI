af3f5bd225ceaad98832db880177d2b326758d03
Rishusite <112302560+Rishusite@users.noreply.github.com>
2024-07-22 16:43:10 +0530

    Update Downloads.jsx

--- client/src/components/Downloads.jsx
+++ client/src/components/Downloads.jsx
@@ -63,7 +63,7 @@ async function gettingpdf(){
 };
 async function createAndDownloadPdf(data,sub){
   axios.defaults.withCredentials = true;
-  await axios.get('https://quizapp-server-one.vercel.app/fetch-pdf')
+  await axios.get('https://quizapp-server-one.vercel.app/fetch-pdf', { responseType: 'blob' })
     .then((res) => {
       const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
         saveAs(pdfBlob, 'smartquizai.pdf');
